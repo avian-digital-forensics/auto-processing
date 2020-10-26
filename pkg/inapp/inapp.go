25efbd4a6cc330dca5ebdd7df5de89a29729313f
simonjanss <simon.jansson1@hotmail.com>
2020-10-26 08:24:14 +0100

    Changed settings for in-app scripts

--- pkg/inapp/inapp.go
+++ pkg/inapp/inapp.go
@@ -7,15 +7,15 @@ import (
 )
 
 type Settings struct {
-	ArchivedPrefix                     string `yaml:"archived_prefix" json:"archived_prefix"`
-	ArchivedTag                        string `yaml:"archived_tag" json:"archived_tag"`
-	ArchivedHasDuplicateTag            string `yaml:"archived_has_duplicate_tag" json:"archived_has_duplicate_tag"`
-	ArchivedMissingDuplicateTag        string `yaml:"archived_missing_duplicate_tag" json:"archived_missing_duplicate_tag"`
-	HasMissingAttachmentsTag           string `yaml:"has_missing_attachments_tag" json:"has_missing_attachments_tag"`
-	ExcludeArchivedItemsWithDuplicates string `yaml:"exclude_archived_items_with_duplicates" json:"exclude_archived_items_with_duplicates"`
-	MainDirectory                      string `yaml:"main_directory" json:"main_directory"`
-	MetadataKey                        string `yaml:"metadata_key" json:"metadata_key"`
-	SourcePath                         string `yaml:"source_path" json:"source_path"`
+	ArchivedPrefix                     string `yaml:"archived_prefix" json:"archived_prefix,omitempty"`
+	ArchivedTag                        string `yaml:"archived_tag" json:"archived_tag,omitempty"`
+	ArchivedHasDuplicateTag            string `yaml:"archived_has_duplicate_tag" json:"archived_has_duplicate_tag,omitempty"`
+	ArchivedMissingDuplicateTag        string `yaml:"archived_missing_duplicate_tag" json:"archived_missing_duplicate_tag,omitempty"`
+	HasMissingAttachmentsTag           string `yaml:"has_missing_attachments_tag" json:"has_missing_attachments_tag,omitempty"`
+	ExcludeArchivedItemsWithDuplicates bool   `yaml:"exclude_archived_items_with_duplicates" json:"exclude_archived_items_with_duplicates,omitempty"`
+	MainDirectory                      string `yaml:"main_directory" json:"main_directory,omitempty"`
+	MetadataKey                        string `yaml:"metadata_key" json:"metadata_key,omitempty"`
+	SourcePath                         string `yaml:"source_path" json:"source_path,omitempty"`
 }
 
 func Config(path string, cfg *Settings) error {
